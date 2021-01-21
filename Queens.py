e236ff58b2b3f37b564d43a9f350c25e8032969b
AlexandrVeziryan <75020608+AlexandrVeziryan@users.noreply.github.com>
2021-01-21 14:44:29 +0400

    Queens

---
+++ Queens.py
@@ -0,0 +1,34 @@
+#_~-=%
+def valid(arr, x, y):
+    for i in range(y, -1, -1):
+        if arr[x][i] == 1:
+            return False
+
+    for i, j in zip(range(x, -1, -1), range(y, -1, -1)):
+        if arr[i][j] == 1:
+            return False       
+
+    for i, j in zip(range(x, n), range(y, -1, -1)):
+        if arr[i][j] == 1:
+            return False       
+    return True
+
+def solve(arr, y = 0):
+    if y == n:
+        return True
+    for x in range(n):
+        if valid(arr, x, y):
+            arr[x][y] = 1
+            if solve(arr, y + 1):
+                return True
+            arr[x][y] = 0
+    return False        
+         
+
+
+
+import numpy as np 
+n = int(input("Enter the size of matrix: "))
+arr = np.zeros((n, n), dtype=int)
+print(solve(arr))
+print(arr)
